85d1d0f4088e2f387c712178928658b031382f8d
CollinsRollinsDev <collinsrollinsmail.com>
2021-09-20 04:45:13 -0700

    added event array

---
+++ components/Events/Event.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Linking,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+const Event = () => {
+    return (
+        <View style={styles.body}>
+            
+        </View>
+    )
+}
+
+export default Event
+
+const styles = StyleSheet.create({
+    body: {
+        minHeight: "100%"
+    }
+})
