b88e18148e2c17b512177fcd054173e2a41440c1
CollinsRollinsDev <collinsrollinsmail.com>
2021-09-21 12:36:34 -0700

    added post

--- components/Landing/Nested/AddNote.js
+++ components/Landing/Nested/AddNote.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Header from "../../Header/Header";
+import Textarea from 'react-native-textarea';
 
 import {
   StyleSheet,
@@ -23,21 +24,39 @@ const Note = () => {
 const { currentTitle, currentPostBody, currentMinistering} =
     useSelector((state) => state.useTheReducer);
 
+    const [addTitle, setAddTitle] = useState()
+    const [addMinistering, setAddMinistering] = useState()
+    const [addPost, setAddPost] = useState()
+
   useEffect(() => {
     LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
   }, []);
 
   const handleChapterPress = async (event) => {};
+  const handleSave = async() => {
+      console.log("working")
+  }
 
   return (
     <View style={styles.body}>
       <Header name="Add a Note" leftSide="Search" />
         <View style={styles.notePreviewContainer}>
-        <TextInput style={styles.input} placeholder="Title Here...." />
-        <TextInput style={styles.input} placeholder="Ministering Here...." />
-        <TextInput multiline={true} style={styles.inputPost} placeholder="Start Adding Note Here...." />
-
+        <TextInput onChangeText={(e) => setAddTitle(e)} style={styles.input} placeholder="Title Here...." />
+        <TextInput onChangeText={(e) => setAddMinistering(e)} style={styles.input} placeholder="Ministering Here...." />
+        <Textarea
+            containerStyle={styles.textareaContainer}
+            style={styles.inputPost}
+            onChangeText={(e) => setAddPost(e)}
+            // defaultValue={"here"}
+            // maxLength={120}
+            placeholder={"Start Adding Note Here...."}
+            placeholderTextColor={'#5661db'}
+            underlineColorAndroid={'transparent'}
+  />
         </View>
+        <TouchableOpacity onPress={handleSave} style={styles.saveBtn}>
+            <Text style={styles.content}>Save Post</Text>
+        </TouchableOpacity>
     </View>
   );
 };
@@ -71,6 +90,27 @@ inputPost:{
     width: '100%',
     // minHeight: 400,
     textAlign: 'left',
+    textAlignVertical: 'top',
     fontSize: 17,
-}
+    marginTop: 20,
+},
+saveBtn: {
+    backgroundColor: 'blue',
+    height: 40,
+    width: 130,
+    borderRadius:20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: "90%",
+    right: '5%',
+},
+content: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+},
+// textareaContainer: {
+//     lineHeight: 500
+// }
 });
